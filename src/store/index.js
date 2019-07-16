0f14fa816c7f08bf52ac314ae86310616988592d
Evgeny Shenets <evgenyshenets91@gmail.com>
2019-07-17 00:38:59 +0300

    test commit

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,11 @@ import createRootReducer from '../reducer';
 import { routerMiddleware } from "connected-react-router";
 import history from "../history";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from 'redux-devtools-extension' 
-import randomId from '../middleware/randomId';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 const store = createStore(createRootReducer(history), composeWithDevTools(applyMiddleware(
   thunk,
-  // randomId,
   routerMiddleware(history)
   ))
 )
